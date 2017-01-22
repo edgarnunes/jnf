4c96dc8e97b4561a3a5b4ea11e1bb4ee073b1001
Luis Alves <lmalves@gmail.com>
2017-01-22 12:46:32 +0000

    Dual select + mostrar produtos do rest

--- app/core/phone/phone.service.js
+++ app/core/phone/phone.service.js
@@ -5,18 +5,32 @@ angular.
   factory('Phone', ['$resource',
     function($resource) {
   //   return $resource('phones/:nomeFile.json', {}, {
-      return $resource('api.php/:nomeFile?transform=1', {}, {
+      return $resource('api.php/:nomeFile?:opcoes', {}, {
+  //  return $resource('api.php/:nomeFile', {}, {
         query: {
           method: 'GET',
-          params: {nomeFile: 'phones'},
+          params: {nomeFile: 'phones', opcoes: 'transform=1'},
           isArray: true
         },
         query_marcas: {
           method: 'GET',
-          params: {nomeFile: 'marcas'},
+          params: {nomeFile: 'marcas', opcoes: 'transform=1'},
 
          //comentar para funcionar com o objeto 
       //  isArray: true
+        } ,
+        query_modelos: {
+          method: 'GET',
+          //params: {nomeFile: 'modelos', opcoes: 'transform=1&filter=modelos.id_marca,eq,4'}
+          params: {nomeFile: 'modelos', opcoes: 'transform=1'}
+
+         //comentar para funcionar com o objeto 
+      //  isArray: true
+        },
+        query_equipamentos : {
+          method: 'GET',
+          params: {nomeFile: 'equipamentos', opcoes: 'transform=1&include=marcas,modelos'} 
+          //isArray: true         
         }        
       });
     }
--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -3,16 +3,25 @@
 // Register `phoneList` component, along with its associated controller and template
 angular.
   module('phoneList').
-  component('phoneList', {
+  component('phoneList',{
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['Phone',
-      function PhoneListController(Phone) {
+    controller: ['Phone','$scope',
+      function PhoneListController(Phone, $scope) {
         //this.phones = Phone.query();
         this.marcas = Phone.query_marcas();
-        console.log("---",Phone.query_marcas());
+        
+        //console.log("---",Phone.query_marcas());
         this.orderProp = 'age';
+        //console.log(this);       
 
+        this.getMarcas = function(){ 
+          console.log(this.marca_escolhida); 
+          this.modelos = Phone.query_modelos(this.marca_escolhida);
+          //return 2222;
+        }
 
+        this.equipamentos = Phone.query_equipamentos();
+        console.log(this.equipamentos.equipamentos);
       }
     ]
   });
